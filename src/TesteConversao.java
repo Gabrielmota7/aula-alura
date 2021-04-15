7305447bda5b057d4a3c3c96c803dc4db03a64ec
gabrielmota7 <motagabriel819@gmail.com>
2021-04-15 16:46:35 -0300

    Aula variaveis

---
+++ src/TesteConversao.java
@@ -0,0 +1,17 @@
+
+public class TesteConversao {
+
+	public static void main(String[] args) {
+		double salario = 1270.50;
+		int valor =  (int) salario;
+		System.out.println(valor);
+		
+		long NumeroGrande = 3231315462643l;
+		double valor1 = 0.2;
+		double valor2 = 0.1;
+		double total = valor1+valor2;
+		
+		System.out.println(total);
+	}
+	
+}
