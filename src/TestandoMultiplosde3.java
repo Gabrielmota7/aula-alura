63c8eedcdabf99b65c6ec1042854eb4408c525d0
gabrielmota7 <motagabriel819@gmail.com>
2021-04-17 14:23:54 -0300

    usando % para achar divisiveis por 3

---
+++ src/TestandoMultiplosde3.java
@@ -0,0 +1,11 @@
+
+public class TestandoMultiplosde3 {
+	public static void main(String[] args) {
+		for(int multiplicador = 3; multiplicador < 100; multiplicador++) {
+			if (multiplicador % 3 == 0) {
+				System.out.println(multiplicador);
+			}
+		}
+	}
+
+}
